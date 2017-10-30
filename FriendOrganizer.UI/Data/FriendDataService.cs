85d48c4086e0e55c5041fe58d9e25eacb5cee764
Jesper Ljungberg <jeetrix1991@gmail.com>
2017-10-30 15:30:59 +0100

    #Modul 4 Created a dataService, a MainViewModel with a base (INotifyProperyChanged), and setting the dataContext of MainWindow

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Jesper", LastName = "Ljungberg" };
+            yield return new Friend { FirstName = "Mikael", LastName = "Hentschel" };
+            yield return new Friend { FirstName = "Folke", LastName = "Ljungberg" };
+            yield return new Friend { FirstName = "Jonas", LastName = "Adamsson" };
+        }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string properyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(properyName));
+        }
+    }
+}
+
+
+
+
