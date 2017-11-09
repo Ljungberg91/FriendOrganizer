5acee0d9921b19222efc73bfedd31967652f9b16
Jesper Ljungberg <jeetrix1991@gmail.com>
2017-11-09 16:50:45 +0100

    #Modul 13 Added a new entity called FriendPhoneNumber, and added some hardcoded data into the SeedMethod. Also added a new Dbset in DbContext

--- FriendOrganizer.DataAccess/Migrations/Configuration.cs
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -28,6 +28,11 @@ namespace FriendOrganizer.DataAccess.Migrations
                 new ProgrammingLanguage { Name = "F#" },
                 new ProgrammingLanguage { Name = "Swift" },
                 new ProgrammingLanguage { Name = "Java" });
+
+            context.SaveChanges();
+
+            context.FriendPhoneNumbers.AddOrUpdate(pn => pn.Number,
+                new FriendPhoneNumber { Number = "+46 0702274187", FriendId = context.Friends.First().Id });
         }
     }
 }
