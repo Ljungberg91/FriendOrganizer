942a1ae414216dacb318c9e37c4cbf7574a75210
Jesper Ljungberg <jeetrix1991@gmail.com>
2017-11-10 18:35:44 +0100

    #Modul 15 Created a meetingRepository with an interface. Created An interface to LookupDataService to implement from, and added a method for getting Meetings. Registrated the DetailViewModels with Autofac IIndex. Created a view and a ViewModel for meetings, and a wrapper.

--- FriendOrganizer.UI/ViewModel/MainViewModel.cs
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using FriendOrganizer.UI.Event;
+﻿using Autofac.Features.Indexed;
+using FriendOrganizer.UI.Event;
 using FriendOrganizer.UI.View.Services;
 using Prism.Commands;
 using Prism.Events;
@@ -11,7 +12,7 @@ namespace FriendOrganizer.UI.ViewModel
     public partial class MainViewModel : ViewModelBase
     {
         private IEventAggregator _eventAggregator;
-        private Func<IFriendDetailViewModel> _friendDetailViewModelCreator;
+        private IIndex<string, IDetailViewModel> _detailViewModelCreator;
         private IMessageDialogService _messageDialogService;
         private IDetailViewModel _detailViewModel;
 
@@ -29,12 +30,11 @@ namespace FriendOrganizer.UI.ViewModel
         }
 
 
-        public MainViewModel(INavigationViewModel navigationViewModel, Func<IFriendDetailViewModel> friendDetailViewModelCreator,
+        public MainViewModel(INavigationViewModel navigationViewModel, IIndex<string, IDetailViewModel> detailViewModelCreator,
             IEventAggregator eventAggregator, IMessageDialogService messageDialogService)
         {
             _eventAggregator = eventAggregator;
-
-            _friendDetailViewModelCreator = friendDetailViewModelCreator;
+            _detailViewModelCreator = detailViewModelCreator;
             _messageDialogService = messageDialogService;
             _eventAggregator.GetEvent<OpenDetailViewEvent>()
                 .Subscribe(OnOpenDetailView);
@@ -62,12 +62,7 @@ namespace FriendOrganizer.UI.ViewModel
                 }
             }
 
-            switch (args.ViewModelName)
-            {
-                case nameof(FriendDetailViewModel):
-                    DetailViewModel = _friendDetailViewModelCreator();
-                    break;
-            }
+            DetailViewModel = _detailViewModelCreator[args.ViewModelName];
             await DetailViewModel.LoadAsync(args.Id);
         }
 
